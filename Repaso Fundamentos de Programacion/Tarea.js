a3786614f1924fe8c7b995fe4cd4d5aefeb92535
Jesus <jesus.andres.pinopino2005@gmail.com>
2023-04-21 08:36:03 -0400

    siu

---
+++ Repaso Fundamentos de Programacion/Tarea.js
@@ -0,0 +1,18 @@
+// 1 Leer los datos de una persona (Nombre, apellidos, edad) y mostrarlos
+// a través de una impresión.
+
+var Nombre = prompt("Por favor ingresar nombre: ");
+
+console.log("Mi nombre es: " + Nombre);
+
+var Apellido = prompt("Por favor ingresar apellido: ")
+
+console.log("Mi apellido es: " + Apellido)
+
+var Edad = prompt("Por favor ingresar edad: ")
+
+console.log("Mi edad es: " + Edad)
+
+// Leer un número, dividirlo en 3 partes, sumar 10 y multiplicarlo por 2,
+// luego mostrar el resultado (por pantalla y por impresora)
+
